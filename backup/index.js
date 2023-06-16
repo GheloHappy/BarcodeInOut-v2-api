3a3d41c3eb04bcf5500342158cb5a91f82fce74c
Ghelonico <ghelonicomaligaya111@gmail.com>
2023-06-16 15:39:56 +0800

    init

---
+++ backup/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const sql = require('mssql');
+
+const config = {
+    user: 'sa',
+    password: 'Passw0rd',
+    server: '192.168.1.248',
+    database: 'MLDIAPP',
+    cryptoCredentialsDetails: {
+        minVersion: 'TLSv1'
+    },
+    options:{
+        trustedConnection: true,
+        encrypt: false
+    }
+};
+
+app.get('/api/v1/getDtSummary', (req, res) => {
+    sql.connect(config, err => {
+        if (err) console.log(err);
+
+        const request = new sql.Request();
+        request.query("SELECT * FROM barcodesys_summary_of_delivery_api", (err, result) => {
+            if (err) console.log(err);
+
+            res.send(result.recordset);
+        });
+    });
+});
+
+app.get('/api/v1/getDtSummaryByDate/:date', (req, res) => {
+    const date = req.params.date;
+    sql.connect(config, err => {
+        if (err) console.log(err);
+        const request = new sql.Request();
+        request.input('date', date);
+        request.query("SELECT * FROM barcodesys_summary_of_delivery_api WHERE QtyShip != 0 AND OrdDate = @date", (err, result) => {
+            if (err) console.log(err);
+
+            res.send(result.recordset);
+        });
+    });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
